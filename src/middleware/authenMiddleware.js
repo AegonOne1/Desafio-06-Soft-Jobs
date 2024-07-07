3e48d7b5246ae4865319ece308bde1a2ca6c916c
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-07-07 15:26:43 -0400

    implementacion del authenMiddleware, con ayuda....

--- src/middleware/authenMiddleware.js
+++ src/middleware/authenMiddleware.js
@@ -1,26 +1,31 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
 const authenMiddleware = (req, res, next) => {
-    const token = req.header("Authorization");
+    const token = req.header('Authorization');
 
     if (!token) {
-        return res.status(401).json({ error: "No se proporciona ningún token" });
+        return res.status(401).json({ error: 'No se proporciona ningún token' });
     }
 
     try {
-        const secretKey = process.env.JWT_SECRET || "default_secret";
-        console.log("Token recibido:", token);
+        const secretKey = process.env.JWT_SECRET || 'default_secret';
+        console.log('Token recibido:', token);
 
-        const decoded = jwt.verify(token, secretKey);
-        console.log("Token decodificado:", decoded);
+        // Asegúrate de que el token tenga el formato 'Bearer token'
+        const tokenParts = token.split(' ');
+        if (tokenParts.length !== 2 || tokenParts[0] !== 'Bearer') {
+            throw new Error('Formato de token inválido');
+        }
 
-        req.user = decoded;
-        next();
+        const decoded = jwt.verify(tokenParts[1], secretKey);
+        console.log('Token decodificado:', decoded);
+
+        req.user = decoded; // Almacena la información decodificada del usuario en req.user
+        next(); // Llama al siguiente middleware o ruta si la verificación es exitosa
     } catch (error) {
-        console.error("Error al verificar el token:", error);
-        res.status(401).json({ error: "Token inválido" });
+        console.error('Error al verificar el token:', error.message);
+        return res.status(401).json({ error: 'Token inválido' });
     }
 };
-
 
 export { authenMiddleware };
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -5,9 +5,13 @@ import { authenMiddleware } from "../middleware/authenMiddleware.js";
 
 const router = express.Router();
 
-
+// Ruta para crear un nuevo usuario
 router.post("/usuarios", createNewUser);
+
+// Ruta para iniciar sesión
 router.post("/login", loginUser);
+
+// Ruta para obtener información de usuarios autenticados, con autenticación requerida
 router.get("/usuarios", authenMiddleware, getAuthenticatedUser);
 
 export default router;
